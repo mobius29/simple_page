aa78f33812c97cc7f6b586d2f35a92cd3edc5f24
mobius29 <sagjin0000@naver.com>
2021-11-07 14:47:58 +0900

    initial settings

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.set('views', `${__dirname}/../views`);
+app.set('view engine', 'pug');
+
+app.use('/', express.static(`${__dirname}/../public`));
+app.use(express.urlencoded({ extended: true}));
+
+app.get('/', (req, res) => res.send("Hello World"));
+
+module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+const app = require('./app');
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`SIMPLE_PAGE: Listening on port ${port}.`);
+});
